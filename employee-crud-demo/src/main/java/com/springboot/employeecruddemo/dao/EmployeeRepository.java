c5dc29d82675a45edeaf34a038e3e9f60f5977e2
Andres Andrade <a.andrade09@yahoo.com>
2020-01-27 18:57:59 -0800

    Updated list-employees form with an "add employee" button. Added employee-form for populating the form with employee fields to add a new employee. Updated employee repo & service to find and display employees ordered by last name. Updated employee controller to show a form for adding an employee and saving an employee into the db.

--- employee-crud-demo/src/main/java/com/springboot/employeecruddemo/dao/EmployeeRepository.java
+++ employee-crud-demo/src/main/java/com/springboot/employeecruddemo/dao/EmployeeRepository.java
@@ -4,11 +4,16 @@ import com.springboot.employeecruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 // Version #3 DAO Technique ...  using spring data JPA
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // boilerplate crud methods inherited for free ... so no extra work needed
     // also, no need for an implementation class
+
+    // add a method to sort by last name
+    public List<Employee> findAllByOrderByLastNameAsc();
 
 }
